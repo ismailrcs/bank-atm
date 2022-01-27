12d4ebc13cb08a330a7838cce1e2bcee9388debc
ismailrcs <69443263+ismailrcs@users.noreply.github.com>
2022-01-27 21:10:16 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,14 @@
+class Atm:
+    def __init__(self,pin,cardNumber):
+        self.pin=pin
+        self.cardNumber=cardNumber
+        #self.balance=balance
+    balance=1400
+    def __casWithdrawl__():
+        amount=input("Enter withdrawl amount")
+        print(amount+"withdrawed")
+        balance=balance-amount
+    def __balanceEnquirey__():
+        print(balance)
+
+
